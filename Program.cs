0543966b6eb484e1325d75c4b922a90a3e39681a
mkrell23 <mkrell23@gmail.com>
2020-08-24 10:51:21 -0400

    Adds json data from D&D 5e API

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.IO;
+using System.Collections.Generic;
 
 namespace NpcGen
 {
@@ -6,6 +10,18 @@ namespace NpcGen
     {
         static void Main(string[] args)
         {
+            var path = (Directory.GetCurrentDirectory() + "\\Data\\");
+            
+            var classes = new List<Classes>();
+            
+            var serializer = new JsonSerializer();
+
+            using (var reader = new StreamReader(path + "Classes.json"))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                 classes = serializer.Deserialize<List<Classes>>(jsonReader);
+            }
+
             Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
