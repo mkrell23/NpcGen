0b7eedbbdeb1be3f707360316435a04da7be0bc3
mkrell23 <mkrell23@gmail.com>
2021-02-15 11:40:34 -0500

    generates class list from API data

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+const charName = document.getElementById('charName');
+const submitButton = document.getElementById('submitButton');
+const charClass = document.getElementById('charClass');
 
 searchApi('classes')
     .then( data => data.results)
@@ -5,7 +8,17 @@ searchApi('classes')
         for (const cName in results) {
             if (Object.hasOwnProperty.call(results, cName)) {
                 const element = results[cName];
-                console.log(element.name);              
+                console.log(element.name);          
+                const classOption = document.createElement('option');
+                classOption.label = element.name;
+                classOption.value = element.name;
+                classOption.text = element.name;
+                charClass.appendChild(classOption);
             }
         }
     });
+
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(charName.value);
+});
