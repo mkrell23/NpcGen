8b6ade47735b53a45b442cfb729b4099ec1342ee
Matt <mkrell23@gmail.com>
2021-02-21 12:23:41 -0500

    this may be a thing

---
+++ characterClass.js
@@ -0,0 +1,10 @@
+class Character {
+    constructor(name, charRace, charClass, charLevel, abilityScores){
+        this.name = name;
+        this.charRace = charRace;
+        this.charClass = charClass;
+        this.charLevel = charLevel;
+        this.baseAbility = abilityScores;
+    }
+
+}
