bb9dee2632d028eb568f380467b3e572f618cac3
Matt <mkrell23@gmail.com>
2021-02-28 08:20:06 -0500

    adding properties from response, lots of TODOS

--- js/characterClass.js
+++ js/characterClass.js
@@ -2,17 +2,47 @@ class Character {
     constructor(charName, charRace, charClass){
         this.charName = charName;
         this.charRace = charRace;
+        this.str = 0;
+        this.dex = 0;
+        this.con = 0;
+        this.cha = 0;
+        this.wis = 0;
+        this.int = 0;
+        this.proficiencies = [];
+        this.saves = [];
         this.charClass = [charClass];
-        searchApi("/api/races/" + this.charRace)
-            .then(response => { 
-                this.raceInfo = response;
-            });
+        this.statsByClass();
+
         searchApi("/api/classes/" + this.charClass)
             .then(response => { 
                 this.classInfo = response;
+
+                this.hitDie = response.hit_die;
+                response.proficiencies.forEach(skill => this.proficiencies.push(skill));
+
+                // TODO: function to chose proficiencies goes here
+
+                response.saving_throws.forEach(save => this.saves.push(save));
+
+                // Saving starting equipment?
+
+                // TODO: Spell things go here
             });
-        this.statsByClass();
-    }
+
+        searchApi("/api/races/" + this.charRace)
+            .then(response => { 
+                this.raceInfo = response;
+
+                response.ability_bonuses.forEach(bonus => this[bonus.ability_score.index] += bonus.bonus);
+
+                // languages? If yes they go here, same pattern as other choices
+                
+                this.size = response.size;
+                this.speed = response.speed;
+                response.starting_proficiencies.forEach(skill => this.proficiencies.push(skill));
+                // TODO : Proficiency choice picking function goes here
+            });
+    };
 
     // Returns a total for one ability score
     statRoll() {
@@ -36,30 +66,41 @@ class Character {
     statsByClass(){
         const abilityScores = this.rollCharacter();
         const charClass = this.charClass[0];
-    
+
         switch (charClass) {
             case "cleric":
-                this.str = abilityScores[0];
-                this.dex = abilityScores[3];
-                this.con = abilityScores[2];
-                this.cha = abilityScores[5];
-                this.wis = abilityScores[1];
-                this.int = abilityScores[4];
+                this.str += abilityScores[0];
+                this.dex += abilityScores[3];
+                this.con += abilityScores[2];
+                this.cha += abilityScores[5];
+                this.wis += abilityScores[1];
+                this.int += abilityScores[4];
                 break;
             
             // BUILD THIS OUT!!!!!!!!!
             default:
-                this.str = abilityScores[0];
-                this.dex = abilityScores[1];
-                this.con = abilityScores[2];
-                this.cha = abilityScores[3];
-                this.wis = abilityScores[4];
-                this.int = abilityScores[5];
+                this.str += abilityScores[0];
+                this.dex += abilityScores[1];
+                this.con += abilityScores[2];
+                this.cha += abilityScores[3];
+                this.wis += abilityScores[4];
+                this.int += abilityScores[5];
                 break;
             }
       
         return this;
     };
+
+    // TODO:
+    //  function to chose class things goes here (classResponse) {
+    //      startingEquipment = response.starting_equipment_options
+    //      startingEquipment.foreach(choice => {
+    //          numberOfChoices = choice.choose
+    //          display the choice.from[{options}, {options}]
+    //          push choices to character object
+    //      })
+    //    }
+
 
     // TODO: better way to do this? Short answer: yes
     displayCharacter() {
@@ -74,4 +115,4 @@ class Character {
         <div class="border-2 border-grey-500 rounded-md">Wisdom: ${this.wis}</div>`;
         return html ;
     };
-}
+};
